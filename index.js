bfb49a04a250aa363a1844f1725fd92800be316d
kinestrikkkk <148617365+kinestrikkkk@users.noreply.github.com>
2023-11-01 20:47:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const valueInputNode = document.querySelector ('.js-value-input');
+const currencySelectorNode = document.querySelector ('.js-currency-selector');
+const outputNode = document.querySelector ('.js-output');
 
+valueInputNode.addEventListener('input', function() {
+    const rub = Number(valueInputNode.value);
+    const currency = currencySelectorNode.value;
+    const result = convert (rub, currency);
+
+    outputNode.innerText = result;
+    console.log(result)
+})
